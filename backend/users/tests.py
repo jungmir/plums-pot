773f7ab814ddc7891aa437363435ebe21f90e85f
syj0396 <syj0396@naver.com>
2024-10-03 23:57:53 +0900

    update: 테스트 개선

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -14,7 +14,7 @@ class UserAPITest(APITestCase):
         self.existing_user = User.objects.create(**self.valid_user_data)
 
     def test_get_all_users_success(self):
-        response = self.client.get('/api/users/')
+        response = self.client.get("/api/users/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_user_success(self):
@@ -58,6 +58,15 @@ class UserAPITest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.existing_user.refresh_from_db()
         self.assertEqual(self.existing_user.username, updated_data["username"])
+
+    def test_user_update_with_invalid_field_failure(self):
+        invalid_data = {
+            "username": "updateeeeeeeeeeeeee!", # 10자로 제한, 특수문자 불가
+            "email": "update@update.com",
+            "password": "test123!"
+        }
+        response = self.client.put(f"/api/users/{self.existing_user.id}/", invalid_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_user_delete_success(self):
         response = self.client.delete(f"/api/users/{self.existing_user.id}/")
